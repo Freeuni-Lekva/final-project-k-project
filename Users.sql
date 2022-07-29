637b04f27f826b1b2dce6e59cd572fb1bcee6732
aniodis <aodis19@freeuni.edu.ge>
2022-07-29 11:03:46 +0400

    database_test

---
+++ Users.sql
@@ -0,0 +1,13 @@
+USE myDatabase;
+
+-- remove table if it already exists and start from scratch
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    user_name CHAR(64)
+    quiz_taken INTEGER,
+);
+
+INSERT INTO users VALUES
+('Mariam', 20),
+('Ani', 50)
